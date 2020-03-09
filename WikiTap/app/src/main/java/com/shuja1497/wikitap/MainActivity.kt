63b87db092e5286f2e0e848bd19be87b14238f3f
shuja1497 <shujareshi1497@gmail.com>
2020-03-09 21:29:53 +0530

    init project

---
+++ WikiTap/app/src/main/java/com/shuja1497/wikitap/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shuja1497.wikitap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
